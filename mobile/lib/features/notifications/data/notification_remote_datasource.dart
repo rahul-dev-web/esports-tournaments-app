4b7031d6131b18ec324d8ff5553a36158a528156
Rahul Kumar Jha <jarahul989@gmail.com>
2026-08-16 19:47:21 +0530

    feat(mobile): add notification datasource

---
+++ mobile/lib/features/notifications/data/notification_remote_datasource.dart
@@ -0,0 +1,21 @@
+import '../../../core/services/api_client.dart';
+import 'notification_models.dart';
+
+class NotificationRemoteDataSource {
+  NotificationRemoteDataSource(this._api);
+
+  final ApiClient _api;
+
+  Future<List<NotificationItem>> fetch({bool unreadOnly = false}) async {
+    final items = await _api.getList('/notifications/me?unread_only=$unreadOnly&limit=100');
+    return items
+        .whereType<Map<String, dynamic>>()
+        .map(NotificationItem.fromJson)
+        .toList();
+  }
+
+  Future<NotificationItem> markRead(String id) async {
+    final response = await _api.patch('/notifications/$id/read');
+    return NotificationItem.fromJson(response as Map<String, dynamic>);
+  }
+}
