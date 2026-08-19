a46d02a4bb1c673546201f7e5319465a157724b6
Rahul Kumar Jha <jarahul989@gmail.com>
2026-08-19 16:23:36 +0530

    fix(auth): add auth provider callback logging import

--- mobile/lib/core/providers/auth_provider.dart
+++ mobile/lib/core/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
